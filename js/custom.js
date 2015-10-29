37ac4c79f0ead7e56de045a6a68a3cc5fed58e28
Ashikul <alam.ashikul@gmail.com>
2015-10-29 17:44:18 -0400

    intial start

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ashikul on 10/20/2015.
+ */
+(function() { var s = document.createElement("script");s.async = true;s.onload = s.onreadystatechange = function(){getYelpWidget("harlem-karibe-takeout-new-york","300","RED","y","y","3");};s.src='http://chrisawren.com/widgets/yelp/yelpv2.js' ;var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);})();
